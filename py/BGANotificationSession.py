97240ee150d9053971cde15f76362390432ede2e
David Rose <david@ddrose.com>
2021-12-28 11:51:30 -0800

    notifications are working well now

--- py/BGANotificationSession.py
+++ py/BGANotificationSession.py
@@ -5,33 +5,43 @@ import websocket         # python -m pip install websocket-client
 import threading
 import time
 import ssl
+import traceback
+import queue
 
 class BGANotificationSession:
-    eio = '3'  # Don't know what this is really?
+    """ Creates a connection to the BGA server to listen for
+    notifications on any number of subscribed channels.  Notifications
+    are delivered in the main thread to the message_callback function
+    passed to the constructor, when the dispatch() method is
+    called. """
+
+    eio = '3'  # Don't know what this is really, but BGA passes it on every URL.
 
     # Messages
-    client_greeting = '2probe'  # Not really sure what this means
+    client_probe_response_msgid = 5 # Not really sure what this means either, but BGA seems to expect it in response to a probe.
 
-    server_greeting = '3probe' # Not really sure what this means
-    client_server_response = '5' # Not really sure what this means
+    client_ping_msgid = 2
+    server_ping_msgid = 3
+    msg_probe = 'probe'  # This can be attached to a ping and seems to mean additional data is requested.  We reply with client_probe_response_msgid.
 
-    client_heartbeat = '2'
-    server_heartbeat = '3'
+    # The id for most notifications, which is what we really care
+    # about here.
+    notification_msgid = 42
 
-    def __init__(self, bga):
+    def __init__(self, bga, message_callback):
         self.bga = bga
+        self.message_callback = message_callback
 
-        # https://r2.boardgamearena.net/r/?user=86152093&name=drwrose&credentials=4faffebfc39f554ccade3ae252bdc388&EIO=3&transport=polling&t=NtuCSTy&sid=W5CFZFPJAYxZJCXcASub
-        # -> JSON notification update, or "ok"
+        self.ws = None
+        self.ws_thread = None
+        self.ping_thread = None
 
-        # https://r1.boardgamearena.net/3/r/?user=86152093&name=drwrose&credentials=4faffebfc39f554ccade3ae252bdc388&EIO=3&transport=polling&t=NtuCSWp&sid=o-dqzPJc-nnLPN1mAF-3
-        # POST: 31:42["join","/table/ts226845327"]30:42["join","/player/p86152093"]
+        # The ws_thread adds notifications to this queue; dispatch()
+        # pulls them out.
+        self.notification_queue = queue.Queue()
 
-        # websocket connection?
-        #https://r2.boardgamearena.net/r/?user=86152093&name=drwrose&credentials=4faffebfc39f554ccade3ae252bdc388&EIO=3&transport=polling&t=Ntu9pqW
-        # -> 96:0{"sid":"tKSrqutPoxgZKJeMAF2z","upgrades":["websocket"],"pingInterval":25000,"pingTimeout":5000}2:40
-
-        #wss://r1.boardgamearena.net/3/r/?user=86152093&name=drwrose&credentials=4faffebfc39f554ccade3ae252bdc388&EIO=3&transport=websocket&sid=tKSrqutPoxgZKJeMAF2z
+        # Get the session ID (sid) so we can open a websocket and
+        # subscribe to topics for that websocket.
 
         subscribe_url = 'https://r2.boardgamearena.net/r/'
         subscribe_params = {
@@ -48,45 +58,27 @@ class BGANotificationSession:
 
         comm_protocol = messages[0]
         self.sid = comm_protocol['sid']
+
+        # The server also tells us how frequently it will expect a
+        # ping from us.  We need to respect that, or it will drop the
+        # connection.
         self.ping_interval = int(comm_protocol['pingInterval']) # time in ms
         self.ping_timeout = int(comm_protocol['pingTimeout']) #?
-        self.ws_thread = None
-        self.heartbeat_thread = None
-
-        # Now start a websocket connection.  As a hack around the fact
-        # that the requests module refuses to format parameters for
-        # non-http URLs, we pass the URL as https originally, then
-        # change it to wss below.
-        websocket_url = 'https://r2.boardgamearena.net/r/'
-        websocket_params = {
-            'user' : self.bga.user_id,
-            'name' : self.bga.user_name,
-            'credentials' : self.bga.socketio_credentials,
-            'EIO' : self.eio,
-            'transport' : 'websocket',
-            'sid' : self.sid,
-            }
-
-        parser = requests.PreparedRequest()
-        parser.prepare_url(url = websocket_url, params = websocket_params)
-
-        # Change the url from https:// to wss://.
-        url = 'wss' + parser.url[5:]
-        #print(url)
-
-        #websocket.enableTrace(True)
-        self.ws = websocket.WebSocketApp(url,
-                                         on_open = self.__ws_open,
-                                         on_message = self.__ws_message,
-                                         on_error = self.__ws_error,
-                                         on_close = self.__ws_close)
 
         self.start_ws()
 
-        #return
+    def cleanup(self):
+        """ Stops any threads and closes any sockets, in preparation
+        for shutdown. """
 
-        # Now that we've connected to the websocket for notifications,
-        # sign up to certain message channels.
+        self.__stop_ping()
+        self.stop_ws()
+
+    def subscribe_channels(self, *channels):
+        """ Subscribes to one or more channels for notifications.
+        Parameters consist of one or more strings that represent
+        channel names. """
+
         subscribe_url = 'https://r2.boardgamearena.net/r/'
         subscribe_params = {
             'user' : self.bga.user_id,
@@ -97,39 +89,24 @@ class BGANotificationSession:
             'sid' : self.sid,
             }
 
-        subscribe_messages = [
-            (42, ["join","/general/emergency"]),
-            ## (42, ["join","/chat/general"]),
-            ## (42, ["join","/player/p86238453"]),
-            ## (42, ["join","/group/g3781377"]),
-            ## (42, ["join","/tablemanager/global"]),
-            ## (42, ["join","/tablemanager/detailled"]),
-            ## (42, ["join","/tablemanager/globalasync"]),
-            ## (42, ["join",["/group/g6563374"]]),
-            ## (42, ["join","/table/t227216683"]),
-            ]
+        subscribe_messages = []
+        for channel_name in channels:
+            subscribe_messages.append((self.notification_msgid, ["join", channel_name]))
         subscribe_text = self.format_messages(subscribe_messages)
-        print(subscribe_text)
+        #print(subscribe_text)
 
         r = self.bga.session.post(subscribe_url, params = subscribe_params, data = subscribe_text)
-        print(r.url)
-        print(r)
-        print(r.text)
-        import pdb; pdb.set_trace()
+        #print(r.url)
+        if r.status_code != 200:
+            message = "Unable to subscribe to topics: %s" % (r.text)
+            raise RuntimeError(message)
+
+        for channel_name in channels:
+            print("Subscribed to %s" % (channel_name))
+
+        #print(r.text)
 
     def format_messages(self, messages):
-        """ Given a list of (id, object) tuples, formats them into a
-        single string to send as a message. """
-
-        text = self.__format_messages(messages)
-        messages2 = self.__parse_messages(text)
-        if (messages != messages2):
-            print(messages)
-            print(messages2)
-            import pdb; pdb.set_trace()
-        return text
-
-    def __format_messages(self, messages):
         """ Given a list of (id, object) tuples, formats them into a
         single string to send as a message. """
 
@@ -149,19 +126,6 @@ class BGANotificationSession:
         return text
 
     def parse_messages(self, text):
-        """ Returns a list of (id, json) tuples, extracted from the
-        notification response. """
-
-        messages = self.__parse_messages(text)
-        text2 = self.__format_messages(messages)
-        if (text != text2):
-            print(text)
-            print(text2)
-            import pdb; pdb.set_trace()
-
-        return messages
-
-    def __parse_messages(self, text):
         """ Returns a list of (id, json) tuples, extracted from the
         notification response. """
 
@@ -186,7 +150,7 @@ class BGANotificationSession:
 
             # The next character after the id is the beginning of the
             # message data.
-            data_start = m.end(0)
+            data_start = m.end(2)
 
             # The end of the message data is measured from the start
             # of id.
@@ -207,70 +171,197 @@ class BGANotificationSession:
 
         return messages
 
-    def poll(self):
-        """ Should be called from time to time to do some stuff. """
+    def dispatch(self, block = False, timeout = None):
+        """ Processes any pending messages, and dispatches them
+        appropriately.  If block is True, this call will wait up till
+        timeout seconds for a message to come in (actually it can wait
+        a bit longer).  Should be called in the main thread. """
 
-        ## print("Polling.")
-        ## poll_url = 'https://r2.boardgamearena.net/r/'
-        ## poll_params = {
-        ##     'user' : self.bga.user_id,
-        ##     'name' : self.bga.user_name,
-        ##     'credentials' : self.bga.socketio_credentials,
-        ##     'EIO' : self.eio,
-        ##     'transport' : 'polling',
-        ##     'sid' : self.sid,
-        ##     #'t' : 'Ntu9pqW',
-        ##     }
+        try:
+            while True:
+                channel, data = self.notification_queue.get(block = block, timeout = timeout)
+                self.message_callback(channel, data)
+        except queue.Empty:
+            return
 
-        ## print(poll_params)
-        ## r = self.bga.session.get(poll_url, params = poll_params)
-        ## messages = self.parse_messages(r.text)
+    def __ws_message(self, ws, text):
+        """ A new message has come in on the websocket.  This method
+        is called in the ws thread (I think).  Decode the message and
+        dispatch it appropriately. """
 
-        #print(messages)
-        pass
+        try:
+            #print("__ws_message(%s): %s" % (ws, text))
 
-    def __ws_message(self, ws, message):
-        print("__ws_message(%s): %s" % (ws, message))
-        if message == self.server_greeting:
-            ws.send(self.client_server_response)
-            if not self.heartbeat_thread:
-                self.start_heartbeat()
+            # Get the integer message_id from the beginning of the message.
+            pattern = re.compile('([0-9]+)')
+            m = pattern.match(text)
+
+            if m is None:
+                message = 'Unexpected message from BGA: %s' % (text)
+                raise RuntimeError(message)
+
+            msgid = int(m.group(1))
+
+            # The next character after the id is the beginning of any
+            # auxiliary message data.
+            data_start = m.end(1)
+            data_text = text[data_start:]
+
+            if msgid == self.server_ping_msgid:
+                if data_text == self.msg_probe:
+                    # When the server sends us a probe, we should send back
+                    # a probe_response, and start pinging.
+                    self.send_message(self.client_probe_response_msgid)
+                    if not self.ping_thread:
+                        self.__start_ping()
+            elif msgid == self.notification_msgid:
+                # Here comes a notification on one of our subscribed
+                # channels.  The data will be json-encoded.
+                data = json.loads(data_text)
+                self.__raw_notification_received(data)
+
+            else:
+                print("Unknown message id %s %s" % (msgid, text))
+
+        except:
+            print("Exception in message handler")
+            traceback.print_exc()
+            import pdb; pdb.set_trace()
+
+    def __raw_notification_received(self, notification_data):
+        message_type, message_data = notification_data
+        if message_type == 'bgamsg':
+            # Most (all?) messages will be of type bgamsg.  These
+            # have a payload which is further json-encoded.
+            print(repr(message_data))
+            bgamsg_data = json.loads(message_data)
+            channel = bgamsg_data['channel']
+            data = bgamsg_data['data']
+            self.bgamsg_notification_received(channel, data)
+        else:
+            message = 'Unexpected message type %s from BGA: %s' % (message_type)
+            raise RuntimeError(message)
+
+    def bgamsg_notification_received(self, channel, data):
+        #print("thread notification on %s: %s" % (channel, data))
+        with self.bga.notification_cvar:
+            self.notification_queue.put((channel, data))
+            self.bga.notification_cvar.notify()
 
     def __ws_error(self, ws, error):
         print("__ws_error(%s): %s" % (ws, error))
 
+        # Maybe closing the socket when we get an error notification
+        # is a good idea, to help us exit cleanly?
+        self.ws.close()
+
     def __ws_close(self, ws, close_status_code, close_msg):
         print("__ws_close(%s)" % (ws))
 
-        self.stop_heartbeat()
+        self.__stop_ping()
 
     def __ws_open(self, ws):
-        print("__ws_open(%s) vs. %s" % (ws, self.ws))
+        #print("__ws_open(%s) vs. %s" % (ws, self.ws))
 
-        ws.send(self.client_greeting)
+        self.send_message(self.client_ping_msgid, self.msg_probe)
+
+    def send_message(self, msgid, data = None):
+        """ Formats the indicated msgid (and optional data) to the
+        server. """
+
+        if data is None:
+            text = '%d' % (msgid)
+        elif isinstance(data, str):
+            text = '%d%s' % (msgid, data)
+        else:
+            json_text = json.dumps(data)
+            text = '%d%s' % (msgid, json_text)
+
+        #print("send %s" % (repr(text)))
+        self.ws.send(text)
 
     def start_ws(self):
+        """ Open a websocket connection and start the ws thread to
+        monitor it. """
+
+        assert not self.ws
         assert not self.ws_thread
+
+        # We use the requests module to format the URL and its
+        # parameters.  As a hack around the fact that the requests
+        # module refuses to format parameters for non-http URLs, we
+        # pass the URL as https originally, then change it to wss
+        # below.
+        websocket_url = 'https://r2.boardgamearena.net/r/'
+        websocket_params = {
+            'user' : self.bga.user_id,
+            'name' : self.bga.user_name,
+            'credentials' : self.bga.socketio_credentials,
+            'EIO' : self.eio,
+            'transport' : 'websocket',
+            'sid' : self.sid,
+            }
+
+        parser = requests.PreparedRequest()
+        parser.prepare_url(url = websocket_url, params = websocket_params)
+
+        # Change the url from https:// to wss://.
+        url = 'wss' + parser.url[5:]
+        #print(url)
+
+        #websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp(url,
+                                         on_open = self.__ws_open,
+                                         on_message = self.__ws_message,
+                                         on_error = self.__ws_error,
+                                         on_close = self.__ws_close)
+
+
         self.ws_thread = threading.Thread(target = self.__ws_thread_main)
         self.ws_thread.start()
 
+    def stop_ws(self):
+        """ Close the websocket and thread previously created by
+        start_ws(). """
+
+        if not self.ws and not self.ws_thread:
+            # No-op: already closed.
+            return
+
+        # This should wake up the thread.
+        self.ws.close()
+
+        thread = self.ws_thread
+        self.ws_thread = None
+        thread.join()
+
     def __ws_thread_main(self):
+        # Actually, this doesn't seem to run *forever*, just until the
+        # socket is closed.
         self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 
-    def start_heartbeat(self):
-        if self.heartbeat_thread:
-            self.stop_heartbeat()
+        self.ws = None
 
-        self.heartbeat_thread = threading.Thread(target = self.__heartbeat_thread_main)
-        self.heartbeat_thread.start()
+    def __start_ping(self):
+        """ Starts the ping thread to send regular heartbeats to the
+        BGA server so it won't hang up on us. """
 
-    def stop_heartbeat(self):
-        if self.heartbeat_thread:
-            thread = self.heartbeat_thread
-            self.heartbeat_thread = None
+        if self.ping_thread:
+            # No-op, already started.
+            return
+
+        self.ping_thread = threading.Thread(target = self.__ping_thread_main)
+        self.ping_thread.start()
+
+    def __stop_ping(self):
+        """ Stops the ping thread that was started previously. """
+
+        if self.ping_thread:
+            thread = self.ping_thread
+            self.ping_thread = None
             thread.join()
 
-    def __heartbeat_thread_main(self):
-        while self.heartbeat_thread:
-            self.ws.send(self.client_heartbeat)
+    def __ping_thread_main(self):
+        while self.ping_thread:
+            self.send_message(self.client_ping_msgid)
             time.sleep(self.ping_interval / 1000.0)
--- py/BoardGameArena.py
+++ py/BoardGameArena.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup   # python -m pip install beautifulsoup4
 import os
 import time
 import logging
+import threading
 from BGANotificationSession import BGANotificationSession
 
 from http.client import HTTPConnection
@@ -15,6 +16,7 @@ class BoardGameArena:
     def __init__(self):
 
         self.notifications = []
+        self.notification_cvar = threading.Condition()
 
         # Create the web session, which stores all of the login
         # cookies and whatnot.
@@ -22,8 +24,18 @@ class BoardGameArena:
 
         self.login()
 
-        self.create_notification_session()
+        notification = self.create_notification_session(self.__basic_notification)
 
+        # Now we can subscribe to the basic channels.
+        notification.subscribe_channels(
+            ## "/general/emergency",  # Probably we don't need this.
+            ## "/chat/general",  # or this
+            "/player/p%s" % (self.user_id),
+            "/tablemanager/global",
+            "/tablemanager/detailled",
+            "/tablemanager/globalasync",
+            ## "/table/t227216683",
+            )
 
         #https://boardgamearena.com/table/table/tableinfos.html?id=226845327&dojo.preventCache=1640545851803
         # -> JSON data about table and game, doesn't change with turns
@@ -37,6 +49,12 @@ class BoardGameArena:
         # Pushing moves
         #https://boardgamearena.com/4/quarto/quarto/selectPiece.html?number=1&table=226600309&dojo.preventCache=1640462571803
         #https://boardgamearena.com/4/quarto/quarto/placePiece.html?x=1&y=4&table=226600309&dojo.preventCache=1640462661462
+
+    def cleanup(self):
+        """ Stops any threads and closes any sockets, in preparation
+        for shutdown. """
+
+        self.cleanup_notification_sessions()
 
     def login(self):
         """ Log in to Board Game Arena. """
@@ -84,22 +102,41 @@ class BoardGameArena:
 
         # Now we are successfully logged in, with appropriate login
         # cookies stored in self.session.
-        return
+        print("Successfully logged in as %s" % (self.user_name))
 
-    def create_notification_session(self):
+    def create_notification_session(self, message_callback):
         """ Signs up for notifications of events from BGA. """
-        notification = BGANotificationSession(self)
+        notification = BGANotificationSession(self, message_callback)
         self.notifications.append(notification)
+        return notification
 
-    def poll(self):
-        """ Should be called from time to time to do some stuff. """
+    def cleanup_notification_sessions(self):
+        """ Stops and removes all previously created notification
+        sessions. """
 
         for notification in self.notifications:
-            notification.poll()
+            notification.cleanup()
+        self.notifications = []
+
+    def dispatch(self):
+        """ Dispatches all pending notifications. """
+
+        for notification in self.notifications:
+            notification.dispatch()
 
     def serve(self):
         """ Does not return, just waits for stuff to happen. """
         print("Serving.")
-        while True:
-            time.sleep(5)
-            self.poll()
+        try:
+            while True:
+                with self.notification_cvar:
+                    self.notification_cvar.wait(1)
+                    self.dispatch()
+        finally:
+            self.cleanup()
+
+    def __basic_notification(self, channel, data):
+        """ A notification is received on the named channel, one of
+        the ones we subscribed to in the constructor. """
+
+        print("Basic notification on %s: %s" % (channel, data))
